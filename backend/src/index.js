fd8a054cd1acd85f8dd6fdb4a644c59b0c20eb63
sunny-yadav <sunny9405683@gmail.com>
2025-02-20 01:04:01 +0530

    added db connection fucntion

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import { ConnectDB } from './db/index.js';
+import app  from './app.js';
+import { ErrorHandeller } from "./utils/ErrorHandeller.js";
+
+dotenv.config({
+    path:'../.env'
+});
+
+app.use(ErrorHandeller)
+
+const PORT = process.env.PORT || 6000 ;
+
+ConnectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server is Running at Port : ", PORT)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb connection failed ",err)
+});
+
